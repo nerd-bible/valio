22261f10032ae3cab5fbf5a46eff0ee112334ce2
thesmartwon <thesmartwon@pm.me>
2025-10-27 16:44:50 -0400

    nicer publish

--- publish.sh
+++ publish.sh
@@ -5,6 +5,7 @@ TAG_CMD="git describe --tags --abbrev=0 --match=v[0-9]*.[0-9]*.[0-9]*"
 
 git fetch --tags
 VERSION=$($TAG_CMD --exact-match 2>/dev/null || true)
+# is the latest commit missing a version tag?
 if [[ -z $VERSION ]]; then
 	VERSION=$($TAG_CMD 2>/dev/null || echo 'v0.0.0')
 	echo "No manual tag, bumping $VERSION"
@@ -13,7 +14,10 @@ if [[ -z $VERSION ]]; then
 	git push --tags origin master
 fi
 echo "Publishing $VERSION"
-cat <<< $(jq ".version = \"${VERSION:1}\" |\
-	.repository.url = \"git+$GITHUB_SERVER_URL/$GITHUB_REPOSITORY\"" package.json) > package.json
+JIT_JSON=$(cat package.json | jq "
+	.version = \"${VERSION:1}\" |
+	.repository.url = \"git+$GITHUB_SERVER_URL/$GITHUB_REPOSITORY\"
+")
+echo -E "$JIT_JSON" > package.json
 
 bunx npm@latest publish
