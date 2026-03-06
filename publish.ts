54d7f06551e753e50682674d87103c7c8f541aaf
thesmartwon <thesmartwon@pm.me>
2026-03-06 12:15:24 -0500

    no jit package.json

--- publish.ts
+++ publish.ts
@@ -41,11 +41,11 @@ if (!version) {
 }
 
 if (!dry) {
-	console.log("Writing temporary package.json");
-	const pkg = JSON.parse(readFileSync("package.json", "utf8"));
-	pkg.version = version.substring(1);
-	pkg.repository = { url: `git+${repoUrl}` };
-	writeFileSync("package.json", JSON.stringify(pkg, null, 2));
+	// console.log("Writing temporary package.json");
+	// const pkg = JSON.parse(readFileSync("package.json", "utf8"));
+	// pkg.version = version.substring(1);
+	// pkg.repository = { url: `git+${repoUrl}` };
+	// writeFileSync("package.json", JSON.stringify(pkg, null, 2));
 
 	console.log("Publishing to NPM", version);
 	execSync("npm publish --provenance --access public");
